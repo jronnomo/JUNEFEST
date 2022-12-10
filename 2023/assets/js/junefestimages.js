b29bfaa52c0c426d2b8a85857ec5e8078888db69
jronnomo <ggronnii@gmail.com>
2022-12-10 15:53:49 -0500

    fix

--- 2023/assets/js/junefestimages.js
+++ 2023/assets/js/junefestimages.js
@@ -16,22 +16,22 @@ function inputCarouselmages(){
 	})
 }
 
-function inputCarouselmages(){
-	fetch('https://api.imagekit.io/v1/files/?name=IMG_0033.JPG&tr=w-288,h-200', {
-        method: 'GET',
-		mode: 'cors',
-		headers: {'Authorization': `Basic private_GrOyuLAwm73AxBRp0SXDIxRe+Gs=`,
-				  'Access-Control-Allow-Origin': '10.0.0.58:80',
-				  'Access-Control-Request-Method': 'GET',
-				  'Access-Control-Allow-Credentials': 'true'},
-	  })
-	.then(res => res.json())
-	.then(data => {
-		console.log(data)
-	})
-	.catch(err => {
-		console.log(`Error is ${err}`)
-	})
-}
+// function inputCarouselmages(){
+// 	fetch('https://api.imagekit.io/v1/files/?name=IMG_0033.JPG&tr=w-288,h-200', {
+//         method: 'GET',
+// 		mode: 'cors',
+// 		headers: {'Authorization': `Basic private_GrOyuLAwm73AxBRp0SXDIxRe+Gs=`,
+// 				  'Access-Control-Allow-Origin': '10.0.0.58:80',
+// 				  'Access-Control-Request-Method': 'GET',
+// 				  'Access-Control-Allow-Credentials': 'true'},
+// 	  })
+// 	.then(res => res.json())
+// 	.then(data => {
+// 		console.log(data)
+// 	})
+// 	.catch(err => {
+// 		console.log(`Error is ${err}`)
+// 	})
+// }
 
 inputCarouselmages()
