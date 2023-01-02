5c8dff0f956f76e1bd6595f498f9e76dc7a22373
jronnomo <ggronnii@gmail.com>
2023-01-02 11:52:52 -0500

    working on bar map section

--- 2023/assets/js/junefestcustoms.js
+++ 2023/assets/js/junefestcustoms.js
@@ -1,9 +1,11 @@
+//Photos from imagekit
 function inputCarouselmages(){
 	fetch('https://api.imagekit.io/v1/files/?name=IMG_0033.JPG&tr=w-288,h-200', {
         method: 'GET',
 		mode: 'cors',
 		headers: {'Authorization': `Basic cHJpdmF0ZV9Hck95dUxBd203M0F4QlJwMFNYREl4UmUrR3M9Og==`,
-				  'Access-Control-Allow-Origin': '10.0.0.58:80',
+				  'Access-Control-Allow-Origin': 'https://127.0.0.1:5500',
+				  'Origin': 'https://127.0.0.1:5500',
 				  'Access-Control-Request-Method': 'GET',
 				  'Access-Control-Allow-Credentials': 'true'},
 	  })
@@ -15,9 +17,56 @@ function inputCarouselmages(){
 		console.log(`Error is ${err}`)
 	})
 }
-
 inputCarouselmages()
 
+// function testAPI(){
+// 	fetch('https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId=5142&minimum=1&fbclid=IwAR3WRquU2i-p5PBfC62mxhILi_fbcBc8Mc1aea5g_4vTHs7j4S8YXpdGGYE')
+// 	.then(res => res.json())
+// 	.then(data => {
+// 		console.log(data)
+// 	})
+// 	.catch(err => {
+// 		console.log(`Error is ${err}`)
+// 	})
+// }
+// testAPI()
+
+
+
+//Photos from imagekit
+// ImageMap Zooms
+Array.from(document.querySelectorAll(".location")).forEach(location => location.addEventListener("click", zoomIn))
+
+function zoomIn(click){
+	let element = click.target
+	if(element.classList.contains("Courthaus")){
+		console.log(element.classList)
+		console.log(document.querySelectorAll(".Courthaus"))
+		document.querySelector("#Courthaus").classList.toggle('hidden')
+	}
+}
+
+// var detailsBox = document.getElementById('barmap');
+
+// document.addEventListener('mouseover', function (e) {
+//   if (e.target.tagName == 'path') {
+//     var content = e.target.dataset.name;
+//     detailsBox.innerHTML = content;
+//     detailsBox.style.opacity = "100%";
+//   }
+//   else {
+//     detailsBox.style.opacity = "0%";
+//   }
+// });
+
+// window.onmousemove = function (e) {
+//   var x = e.clientX,
+//       y = e.clientY;
+//   detailsBox.style.top = (y + 20) + 'px';
+//   detailsBox.style.left = (x) + 'px';
+// };
+
+// COUNTDOWN
 let juneFestDate = new Date('June 17, 2023 00:00:00').getTime()
 let currentDate = new Date()
 var countDownTimer = setInterval(function(){
